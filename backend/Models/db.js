b981ba238adb2e0ea9dfe4995712f68585fdf8a9
HIMANSHU TIWARI <119152532+himanshutiwari2709@users.noreply.github.com>
2025-02-20 01:19:13 +0530

    simple todo list

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -12,9 +12,20 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Conn Error...", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// Handle connection events
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
